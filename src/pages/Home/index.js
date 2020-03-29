3fea66d977620388c5df5aac8b98bf6d8ae6be64
wellingtonp1 <40180607+wellingtonp1@users.noreply.github.com>
2020-03-28 21:07:52 -0300

    new form UI

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView, View, Text, Button, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
+
 import styles from './styles';
 
 export default function Home({ navigation }) {
