575882f280254fa487974f8c40c867a40cff0552
wellingtonp1 <40180607+wellingtonp1@users.noreply.github.com>
2020-03-31 21:30:06 -0300

    new map UI

--- src/pages/Light/index.js
+++ src/pages/Light/index.js
@@ -1,73 +1,141 @@
-import React from 'react';
-import { View, Text, Button, CheckBox, TextInput } from 'react-native';
+import React,{ useState }  from 'react';
+import { View, Text, Button, TouchableOpacity, Picker, StyleSheet, PermissionsAndroid ,Platform } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { withFormik } from 'formik';
-import Map from '../../components/Map';
-
+import Geolocation from 'react-native-geolocation-service';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 
-const Form = (props) => (
-  <View style={{ flex: 1, backgroundColor: '#2A7549' }}>
+
+import MapView, {Marker} from 'react-native-maps';
+
+export default function Light({ navigation }) {
+  
+  const [position, setPosition] = useState({
+    latitude: 0.0996574,
+    longitude: -51.054168,
+    latitudeDelta: 0.0033,
+    longitudeDelta: 0.0031,
+  });
+  
+  const request_location_runtime_permission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permissão de Localização',
+          message: 'A aplicação precisa da permissão de localização.',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        Geolocation.getCurrentPosition(
+          pos => {
+            setPosition({
+              ...position,
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude,
+            });
+          },
+          error => {
+            console.log(error);
+            Alert.alert('Houve um erro ao pegar a latitude e longitude.');
+          },
+        );
+      } else {
+        Alert.alert('Permissão de localização não concedida');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function navigateToHome() {
+    navigation.navigate('Home');
+  }
+
+  function getMyCurrentCoords() {
+   
+  }
+ 
+  const [selectedValue, setSelectedValue] = useState("true");
+  const [selectedValueIsItWorking, setSelectedValueIsItWorking] = useState("true");
+  return (
+
+  <View style={{ flex: 1, backgroundColor: '#2A7549', padding: 10 }} onLayout={getMyCurrentCoords}>
       <View >
          <Text style={styles.pageTitle} >Aponte Problemas com a iluminação pública</Text>
       </View>
- <View>
+        <View>
+        <View style={{ padding: 10 }}>
+                <Text style={styles.description} >Existe Poste ?</Text>
+                <View style={styles.container}>
+                  <Picker
+                    selectedValue={selectedValue}
+                    style={{ height: 50 }}
+                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+                  >
+                    <Picker.Item label="Sim" value="true" />
+                    <Picker.Item label="Não" value="false" />
+                  </Picker>
+                </View>
+                <Text style={styles.description} >Esta funcionando ?</Text>
+                <View style={styles.container}>
+                  <Picker
+                    selectedValue={selectedValueIsItWorking}
+                    style={{ height: 50 }}
+                    onValueChange={(itemValue, itemIndex) => setSelectedValueIsItWorking(itemValue)}
+                  >
+                    <Picker.Item label="Sim" value="true" />
+                    <Picker.Item label="Não" value="false" />
+                  </Picker>
+                </View>
+                
+            </View>  
+            <TouchableOpacity
+            style={styles.locationButton}
+            onPress={() => {
+              request_location_runtime_permission();
+            }}>
+           
+           <Text style={{marginTop: 40, color: "#fff"}} >Verifique sua localização</Text>
+            <Icon  color={'#fff'} size={30} />
+           
+      </TouchableOpacity>
+            <MapView
+        style={{height: 250}}
+        region={position}
+        onPress={e =>
+          setPosition({
+            ...position,
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+            longitudeDelta: 0.0134,
+            latitudeDelta: 0.0143
+          })
+        }>
+        <Marker
+          coordinate={position}
+          title={'Você esta aqui!'}
+          description={'Usaremos suas coordenadas'}
+        />
+      </MapView>
+     
 
-    <Text style={styles.description} >Existe Poste ?</Text>
-    <TextInput style={ styles.formField }
-      value={props.values.HasLightPole}
-      onChangeText={text => props.setFieldValue('HasLightPole', text)}
-    />
-     <Text style={styles.description} >As luzes estão funcionando ?</Text>
-    <TextInput style={ styles.formField }
-      value={props.values.IsItWorking}
-      onChangeText={text => props.setFieldValue('IsItWorking', text)}
-    />
 
-    <Button color="#F5BA39"
-      onPress={props.handleSubmit}
-      title="Enviar as informações"
-    />
+
+
+      </View >
+      
+        <View style={{marginTop:30}}>
+              <Button color="#F5BA39" title="Enviar" onPress={navigateToHome} /> 
+          </View>
   </View>
-  
-    <Map />
-   
 
-  </View>
-);
 
-export default withFormik({
-  mapPropsToValues: () => ({ HasLightPole: '', HasLightPole: '', Long: '', Lat: '', Description: '' }),
-
-  handleSubmit: (values, { setSubmitting, setErrors }) => {
-    apiService.post('https://lupanhapi.azurewebsites.net/api/light/', values)
-      .then(/* sucesso */)
-      .catch(err => {
-        setSubmitting(false);
-        setErrors({ message: err.message });
-      });
-  }
-})(Form);
 
   
-//  return (
-  //  <View style={{ flex: 1, backgroundColor: '#2A7549' }}>
-   //   <View style={{padding:30}}>
-    //    <ScrollView>
-     //     <View >
-      //     <Text style={styles.pageTitle} >Aponte Problemas com a iluminação pública</Text>
-       //   </View>
-        //  <View style={{marginTop:30}}>
-        //  <Text style={styles.description} >Existe Poste ?</Text>
-       
-     //     <Text style={styles.description} >As luzes estão funcionando ?</Text>
-     //     <View style={{marginTop:30}}>
-      ///        <Button color="#F5BA39" title="Enviar" onPress={navigateToHome} /> 
-        //  </View>
-         
-        //  </View>
-    //    </ScrollView>
-    //  </View>
-    // </View>
- // );
-// }
+  );
+
+  
+
+}
--- src/pages/Light/styles.js
+++ src/pages/Light/styles.js
@@ -74,6 +74,57 @@ const styles = StyleSheet.create({
     width: 345,
     height: 200,
  },
+//
+ map: {
+  height: '100%',
+  width: '100%',
+},
+logo: {
+  backgroundColor: '#fff',
+  borderRadius: 15,
+  paddingHorizontal: 15,
+  elevation: 5,
+  marginTop: -730,
+  alignSelf: 'center',
+  marginRight: 10,
+  flexDirection: 'row',
+},
+logoText: {
+  fontWeight: 'bold',
+  fontSize: 22,
+},
+positonBox: {
+  backgroundColor: '#fff',
+  borderRadius: 20,
+  opacity: 0.75,
+  marginTop: -170,
+  marginHorizontal: 40,
+  padding: 25,
+  shadowColor: '#000',
+  elevation: 5,
+},
+positonBoxTitle: {
+  textAlign: 'center',
+  fontSize: 22,
+  fontWeight: 'bold',
+  color: '#e74c3c',
+},
+positonBoxLatLon: {flexDirection: 'row', justifyContent: 'space-between'},
+locationButton: {
+  backgroundColor: '#F5BA39',
+  borderRadius: 3,
+  marginTop: -15,
+  marginBottom: 10,
+  width: '100%',
+  height: 37,
+  alignSelf: 'center',
+  justifyContent: 'center',
+  alignItems: 'center',
+  shadowColor: '#000',
+  elevation: 8},
+
+
+ //
 });
 
 export default styles;
